d09b6c8d210ef4059cbad8ef9872e7936daf0db1
Amirul Lokman Jamaludin <amirul.l.jamaludin@gmail.com>
2025-05-05 14:06:08 +0800

    feat: track on change

--- frontend/src/content/main.ts
+++ frontend/src/content/main.ts
@@ -1,24 +1,32 @@
-async function createDialog() {
+function createDialog(enabled: boolean) {
   const mainDoc = document.body;
   console.log(mainDoc);
   if (!mainDoc) {
     console.error("Main document element not found");
   }
-
-  const result = await chrome.storage.sync.get(["enabled"]);
-
-  console.log(result);
-
-  if (result.enabled) {
-    console.log("Hello");
-    const dialog = document.createElement("dialog");
-    dialog.innerHTML = `
+  const dialog = document.createElement("dialog");
+  dialog.innerHTML = `
     <h2>Extension Enabled</h2>
     <p>This extension is currently enabled.</p>
   `;
+  if (enabled) {
     mainDoc.appendChild(dialog);
     dialog.showModal();
+  } else {
+    dialog.close();
+    mainDoc.removeChild(dialog);
   }
 }
 
-createDialog();
+chrome.storage.onChanged.addListener((changes, areaName) => {
+  if (areaName === "sync") {
+    createDialog(changes.enabled.newValue);
+  }
+});
+
+const init = async () => {
+  const result = await chrome.storage.sync.get(["enabled"]);
+  createDialog(result.enabled);
+};
+
+init();
